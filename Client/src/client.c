2a919f21f77829cffcb9789e316792c10f4bd414
SergioFoma <s.kuzminsky.2007@gmail.com>
2026-02-11 03:37:11 +0300

    chat in concole in main

---
+++ Client/src/client.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <sys/socket.h>             // lib for socket
+#include <sys/socket.h>             // lib for consts for socket
+#include <netinet/in.h>             // sockaddr_in
+#include <unistd.h>                 // close
+#include <arpa/inet.h>              // htons
+
+#include "paint.h"
+#include "myStringFunction.h"
+
+const size_t port = 27000;
+const size_t sizeOfBuffer = 1024;                       // size of buffer for read from descriptor
+const size_t maxIndexInBuffer = sizeOfBuffer - 1;
+const size_t standardShipping = 0;
+const char* ipAddress = "127.0.0.1";
+
+typedef struct sockaddr_in socketStruct;
+typedef struct sockaddr universalNetworkStruct;
+
+int main(){
+
+    int client_fd = socket( AF_INET, SOCK_STREAM, 0 );
+    if( client_fd == -1 ){
+        colorPrintf( NOMODE, RED, "Client get error of create socket:%s, %s, %d\n", __func__, __FILE__, __LINE__ );
+        return 0;
+    }
+
+    socketStruct server_addr = {};
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    inet_pton( AF_INET, ipAddress, &server_addr.sin_addr );
+
+    socklen_t serverSize = sizeof( server_addr );
+
+    int statusOfConnect = connect( client_fd, (universalNetworkStruct*)&server_addr, serverSize );
+    if( statusOfConnect < 0 ){
+        colorPrintf( NOMODE, RED, "Error of connect client with server:%s, %s, %d\n", __func__, __FILE__, __LINE__ );
+        close( client_fd );
+        return 0;
+    }
+
+    char* buffer = (char*)calloc( sizeOfBuffer, sizeof( char ) );
+    while( true ){
+        colorPrintf( NOMODE, YELLOW, "Write message: " );
+
+        size_t countOfBytes = sizeOfBuffer;
+        size_t messageLen = getlineWrapper( &buffer, &countOfBytes, stdin );
+
+        send( client_fd, buffer, messageLen, standardShipping);
+
+        memset( buffer, 0, sizeOfBuffer );
+        int statusOfReading = recv( client_fd, buffer, sizeOfBuffer, 0 );
+        if( strcmp( "STOP", buffer ) == 0 ){
+            colorPrintf( NOMODE, YELLOW, "Server finish the chat\n" );
+            break;
+        }
+        if( statusOfReading > 0 ){
+            colorPrintf( NOMODE, PURPLE, "Message from server: %s\n", buffer );
+        }
+        memset( buffer, 0, sizeOfBuffer );
+    }
+
+    close( client_fd );
+    free( buffer );
+    return 0;
+}
---
+++ Server/src/server.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <sys/socket.h>                                 // lib for socket
+#include <sys/socket.h>                                 // lib for consts for socket
+#include <netinet/in.h>                                 // sockaddr_in
+#include <unistd.h>                                     // close
+#include <arpa/inet.h>                                  // htons
+
+
+#include "paint.h"
+#include "myStringFunction.h"
+
+const size_t port = 27000;                              // number of port, for console
+const int backLog = 5;                                  // connection queue
+const size_t sizeOfBuffer = 1024;                       // size of buffer for read from descriptor
+const size_t maxIndexInBuffer = sizeOfBuffer - 1;
+
+typedef struct sockaddr_in socketStruct;
+typedef struct sockaddr universalNetworkStruct;
+
+int main(){
+
+    socketStruct server_addr;                              // struct describe socket for protocol
+    int server_fd = socket( AF_INET, SOCK_STREAM, 0 );     // AF_INET -IPv4
+
+    // AF_INET - address IPv4, SOCK_STREAM - reliable communication, 0 - protocol
+    if( server_fd == -1 ){
+        colorPrintf( NOMODE, RED, "Error of get socket:%s, %s, %d\n", __func__, __FILE__, __LINE__ );
+        return 0;
+    }
+
+    server_addr.sin_family = AF_INET;              // console
+    server_addr.sin_port = htons(port);           // number of port
+    server_addr.sin_addr.s_addr = INADDR_ANY;     // binding the socket to all network interfaces
+
+
+    if( bind( server_fd, (universalNetworkStruct*)&server_addr, sizeof(server_addr) ) < 0 ){
+        colorPrintf( NOMODE, RED, "Binding error:%s, %s, %d\n", __func__, __FILE__, __LINE__ );
+        return 0;
+    }
+
+    // Start of listening
+    if( listen( server_fd, backLog ) < 0 ){
+        colorPrintf( NOMODE, RED, "Listen error:%s, %s, %d\n", __func__, __FILE__, __LINE__ );
+        return 0;
+    }
+
+    //Work with client
+    int client_fd;
+    socketStruct client_addr;
+    socklen_t clientSize = sizeof( client_addr );
+
+    colorPrintf( NOMODE, YELLOW, "Connection...\n" );
+
+    // Connection
+    client_fd = accept( server_fd, (universalNetworkStruct*)&client_addr, &clientSize );
+    if( client_fd < 0 ){
+        colorPrintf( NOMODE, RED, "Accept error:%s, %s, %d\n", __func__, __FILE__, __LINE__ );
+    }
+    colorPrintf( NOMODE, GREEN, "Connection client: %s\n", inet_ntoa( client_addr.sin_addr ) );
+
+    char* buffer = (char*)calloc( sizeOfBuffer, sizeof(char) );
+    buffer[ maxIndexInBuffer ] = '\0';
+
+    while( true ){
+        ssize_t statusOfRead = read( client_fd, buffer, maxIndexInBuffer );
+        if( statusOfRead == -1 ){
+            colorPrintf( NOMODE, RED, "Error of read:%s, %s, %d\n", __func__, __FILE__, __LINE__ );
+        }
+        colorPrintf( NOMODE, PURPLE, "Message from client: %s\n", buffer );
+
+        if( strcmp( "STOP", buffer ) == 0 ){
+            colorPrintf( NOMODE, YELLOW, "Client left the chart\n" );
+            break;
+        }
+
+        memset( buffer, 0, sizeOfBuffer );
+
+        colorPrintf( NOMODE, YELLOW, "Write message: " );
+
+        size_t countOfBytes = sizeOfBuffer;
+        size_t messageLen = getlineWrapper( &buffer, &countOfBytes, stdin );
+        write( client_fd, buffer, messageLen );
+        memset( buffer, 0, maxIndexInBuffer );
+    }
+
+    colorPrintf( NOMODE, GREEN, "Server secussful was started on port %lu\n", port );
+    close( server_fd );
+    close( client_fd );
+    free( buffer );
+    return 0;
+}
